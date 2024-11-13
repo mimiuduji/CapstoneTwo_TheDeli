a7fb929e66793336fe8598db0316e7d93cd01473
Your Name <you@example.com>
2024-11-13 07:30:39 -0600

    Worked on order class

--- src/main/java/com/ps/Order.java
+++ src/main/java/com/ps/Order.java
@@ -1,4 +1,21 @@
 package com.ps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public double calculateTotalPrice() {
+        return items.stream().mapToDouble(Item::getPrice).sum();
+    }
+
+    @Override
+    public String toString() {
+        return "Order:\n" + items + "\nTotal: $" + calculateTotalPrice();
+    }
 }
