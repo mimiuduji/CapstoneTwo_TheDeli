57b9d621cdfa0f976bdcdd809ea66d85d2c813a8
Your Name <you@example.com>
2024-11-13 08:54:24 -0600

    Worked on Topping class

--- src/main/java/com/ps/Topping.java
+++ src/main/java/com/ps/Topping.java
@@ -1,4 +1,40 @@
 package com.ps;
 
-public class Topping {
+public abstract class Topping {
+    private String name;
+
+    public Topping(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double getCost();
+}
+
+class RegularTopping extends Topping {
+    public RegularTopping(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getCost() {
+        return 0; // Regular toppings are free
+    }
+}
+
+class PremiumTopping extends Topping {
+    private double cost;
+
+    public PremiumTopping(String name, double cost) {
+        super(name);
+        this.cost = cost;
+    }
+
+    @Override
+    public double getCost() {
+        return cost;
+    }
 }
